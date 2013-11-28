0d581454db7a696c8dfb3358fd755a24ad1b6649
Ngoc Dao <ngocdaothanh@gmail.com>
2013-11-28 15:46:06 +0900

    Update Xitrum from 2.12 to 2.13

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object XitrumMultimoduleDemoBuild extends Build {
     // and it takes several hours to sync from Sonatype to Maven Central
     resolvers += "SonatypeReleases" at "http://oss.sonatype.org/content/repositories/releases/",
 
-    libraryDependencies += "tv.cntt" %% "xitrum" % "2.12-SNAPSHOT",
+    libraryDependencies += "tv.cntt" %% "xitrum" % "2.13",
 
     // Xitrum uses SLF4J, an implementation of SLF4J is needed
     libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.0.13",

db79e5c54c24cf359a9b584810697eb84aa618a7
Takeharu Oshida <takeharu.oshida@mobilus.co.jp>
2013-12-25 20:21:47 +0900

    Update Xitrum from 2.14 to 2.15 https://groups.google.com/forum/#!topic/xitrum-framework/zW0ZlaTUf3o

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object XitrumMultimoduleDemoBuild extends Build {
     // and it takes several hours to sync from Sonatype to Maven Central
     resolvers += "SonatypeReleases" at "http://oss.sonatype.org/content/repositories/releases/",
 
-    libraryDependencies += "tv.cntt" %% "xitrum" % "2.14",
+    libraryDependencies += "tv.cntt" %% "xitrum" % "2.15",
 
     // Xitrum uses SLF4J, an implementation of SLF4J is needed
     libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.0.13",

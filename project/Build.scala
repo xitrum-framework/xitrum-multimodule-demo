20649fd4249e04deb89cbce201ac06fc936c2ca2
Ngoc Dao <ngocdaothanh@gmail.com>
2018-05-25 22:29:29 +1000

    Update Xitrum from 3.28.8 to 3.28.9

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object XitrumMultimoduleDemoBuild extends Build {
     organization := "tv.cntt",
     version      := "1.0.0-SNAPSHOT",
 
-    scalaVersion := "2.12.4",
+    scalaVersion := "2.12.6",
     scalacOptions ++= Seq("-deprecation", "-feature", "-unchecked"),
 
     // Xitrum requires Java 8
@@ -17,7 +17,7 @@ object XitrumMultimoduleDemoBuild extends Build {
 
     //--------------------------------------------------------------------------
 
-    libraryDependencies += "tv.cntt" %% "xitrum" % "3.28.8",
+    libraryDependencies += "tv.cntt" %% "xitrum" % "3.28.9",
 
     // Xitrum uses SLF4J, an implementation of SLF4J is needed
     libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.3",
